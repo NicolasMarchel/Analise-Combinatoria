3bb0affd75b0d1b42b68c2a3089954f8fc7a3904
Nicolas Marchel de Oliveira <59205948+NicolasMarchel@users.noreply.github.com>
2021-06-14 20:23:30 -0300

    Comecei criando uma interface com a assinatura do método que sera usado por todas as classes de analise combinatoria.

---
+++ AnaliseCombinatoria/AnaliseCombinatoria/IAnaliseCombinatoria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnaliseCombinatoria
+{
+	interface IAnaliseCombinatoria
+	{
+		double CalculaOperacao(int N, int P = 0);
+
+	}
+}
---
+++ AnaliseCombinatoria/AnaliseCombinatoria/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AnaliseCombinatoria
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+
+
+
+		}
+	}
+}
