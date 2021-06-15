9ff4630b30e36dd6d9dc5da91db0c7cce5204a05
Nicolas Marchel de Oliveira <59205948+NicolasMarchel@users.noreply.github.com>
2021-06-14 21:00:56 -0300

    Criei uma classes chamada calculo, ela contém um método que recebe um objeto AnaliseCombinatoria e efetua a operação. Ou seja, ela recebera qualquer objeto herdeiro de analise combinatoria e efetuara a operação que foi definida nele.

---
+++ AnaliseCombinatoria/AnaliseCombinatoria/Calculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnaliseCombinatoria
+{
+	class Calculo
+	{
+		public double EfetuaOperacao(AnaliseCombinatoria X, int N, int P=0)
+		{
+			return X.CalculaOperacao(N, P);
+		}
+	}
+}
